fb30367c9d15be86492a87347d04035dec2ee104
MeAnnaLuiza <anna.luiza.9919@gmail.com>
2024-06-14 00:21:54 -0300

    Fluxo principal do código

--- main.c
+++ main.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include "ArvoreLLRB.h"
 int main(){
-    ArvLLRB * raiz = cria_ArvLLRB();
+    ArvLLRB * raiz = criar_arvore();
 
     int i, N = 9, val[9] = {11,2,1,5,4,7,8,14,15};
 
     for(i=0; i< N; i++){
-        insere_ArvLLRB(raiz,val[i]);
+        inserir_novo_no(raiz,val[i]);
     }
 
-    emOrdem_ArvLLRB(raiz,0);
+    percorrer_em_ordem(raiz,0);
 
     printf("\n\n==========================\n\n");
 
-    remove_ArvLLRB(raiz,4);
-    emOrdem_ArvLLRB(raiz,0);
+    remover_no_arvore(raiz,4);
+    percorrer_em_ordem(raiz,0);
     printf("\n\n==========================\n\n");
-    remove_ArvLLRB(raiz,1);
-    emOrdem_ArvLLRB(raiz,0);
+    remover_no_arvore(raiz,1);
+    percorrer_em_ordem(raiz,0);
 
     return 0;
 }
